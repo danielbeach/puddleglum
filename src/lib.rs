7b7410bf4d2559c73f0d91e8a85ecdd26fdbadc6
Daniel B <34192225+danielbeach@users.noreply.github.com>
2024-05-25 10:04:25 -0500

    Adding methods to list all objects, get latest, see if latest is empty

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,16 @@ use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::Client;
 use aws_sdk_s3::config::Credentials;
 use std::env;
+use std::future::IntoFuture;
 use aws_sdk_s3::primitives::DateTime;
 
 pub struct S3 {
     pub bucket: String,
-    pub key: String,
+    pub prefix: String,
     pub client: Option<Client>,
 }
 
+#[derive(Debug)]
 pub struct S3object {
     pub key: Option<String>,
     pub last_modified: Option<DateTime>,
@@ -43,19 +45,26 @@ impl S3 {
         self.client = Some(client);
     }
 
-    pub async fn list_objects(client: &Client, bucket: &str, bucket_contents: Vec<&Object>) ->Vec<&Object> {
+    pub async fn get_em(client: &Client, bucket: &str, prefix: &str) -> Vec<S3object> {
         let mut response = client
             .list_objects_v2()
+            .prefix(prefix)
             .bucket(bucket.to_owned())
-            .max_keys(10)
+            .max_keys(50)
             .into_paginator()
             .send();
-    
+        let mut all_results: Vec<S3object> = Vec::new();
+
         while let Some(result) = response.next().await {
             match result {
                 Ok(output) => {
                     for object in output.contents() {
-                        bucket_contents.push(object);
+                        let s3object = S3object {
+                            key: object.key().map(String::from),
+                            last_modified: object.last_modified().cloned(),
+                            size: object.size(),
+                        };
+                        all_results.push(s3object);
                     }
                 }
                 Err(err) => {
@@ -64,10 +73,42 @@ impl S3 {
             }
         }
     
-        bucket_contents
+        return all_results;
     }
-    
-    
 
+    pub async fn list_objects(&self) -> Vec<S3object> {
+        let client = self.client.as_ref().expect("client not found");
+        let bucket = &self.bucket;
+        let prefix = &self.prefix;
+        return S3::get_em(client, bucket, prefix).await;
+    }
 
+    pub async fn get_most_recent_file(&self) -> S3object {
+        let objects = self.list_objects();
+        let mut most_recent = S3object {
+            key: None,
+            last_modified: None,
+            size: None,
+        };
+        for object in objects.await {
+            if most_recent.last_modified.is_none() {
+                most_recent = object;
+            } else {
+                if object.last_modified.unwrap() > most_recent.last_modified.unwrap() {
+                    most_recent = object;
+                }
+            }
+        }
+        return most_recent;
+    }
+
+    pub async fn is_most_recent_file_empty(&self) -> bool {
+        let most_recent = self.get_most_recent_file();
+        let key = most_recent.await.size;
+        if key.is_none() {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use puddleglum::S3;
 #[tokio::main]
 async fn main() {
     let mut s3object = S3 {
-        bucket: "my-bucket".to_string(),
-        key: "my-key".to_string(),
+        bucket: "some-bucket".to_string(),
+        prefix: "some-prefix".to_string(),
         client: Default::default(),
     };
 
     s3object.build_config_and_client().await;
 
-    
+    // get all the objects in the bucket and prefiex
+    let em = s3object.list_objects().await;
+    println!("{:?}", em);
+
+    // get the most recent object in the bucket and prefix
+    let most_recent = s3object.get_most_recent_file().await;
+    println!("{:?}", most_recent);
 }
